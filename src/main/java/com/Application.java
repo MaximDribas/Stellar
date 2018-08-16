0d2b4af1e0307caf38610d574b2a66fb44149e62
Maxim Dribas <mvdribas@gmail.com>
2018-08-16 14:02:58 +0300

    Finished (except for the transfer of its own token between accounts).

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,6 +1,9 @@
 package com;
 
+import org.stellar.sdk.Asset;
+import org.stellar.sdk.AssetTypeCreditAlphaNum12;
 import org.stellar.sdk.AssetTypeNative;
+import org.stellar.sdk.ChangeTrustOperation;
 import org.stellar.sdk.KeyPair;
 import org.stellar.sdk.Memo;
 import org.stellar.sdk.Network;
@@ -37,15 +40,15 @@ public class Application {
     }
 
     private static void menu(BufferedReader reader, Server server) throws Exception {
-        System.out.println("\nClick: 1 - create an Account, 2 - send money, " +
-                "3 - print account info, 4 - print info about all accounts, 0 - exit the program");
+        System.out.println("\nClick: 1 - create an Account, 2 - send lumens, 3 - print account info, \n" +
+                "4 - print info about all accounts, 5 - create and send an asset, 6 - send custom asset, 0 - exit the program");
         int number = Integer.parseInt(reader.readLine());
         switch (number) {
             case 1:
                 createAccount(server);
                 break;
             case 2:
-                buildTransaction(reader, server);
+                sendLumens(reader, server);
                 break;
             case 3:
                 System.out.println("Enter secret seed of source account:");
@@ -54,6 +57,12 @@ public class Application {
                 break;
             case 4:
                 printAllAccountsInfo(server);
+                break;
+            case 5:
+                createAndSendCustomAsset(reader, server);
+                break;
+            case 6:
+                sendCustomAsset(reader, server);
                 break;
             case 0:
                 reader.close();
@@ -65,7 +74,7 @@ public class Application {
         }
     }
 
-    private static void buildTransaction(BufferedReader reader, Server server) throws IOException {
+    private static void sendLumens(BufferedReader reader, Server server) throws IOException {
         System.out.println("Enter secret seed of source account:");
         String sourceSecretSeed = reader.readLine();
         System.out.println("Enter account id of destination account:");
@@ -95,6 +104,76 @@ public class Application {
             System.out.println("Something went wrong!");
             System.out.println(e.getMessage());
         }
+    }
+
+    private static void sendCustomAsset(BufferedReader reader, Server server) throws IOException {
+        System.out.println("Enter secret seed of source account:");
+        KeyPair sourceKeys = KeyPair.fromSecretSeed(reader.readLine());
+        System.out.println("Enter secret seed of destination account:");
+        KeyPair destinationKeys = KeyPair.fromSecretSeed(reader.readLine());
+        System.out.println("Enter asset code:");
+        String assetCode = reader.readLine();
+        System.out.println("Enter trust limit:");
+        String trustLimit = reader.readLine();
+        System.out.println("Enter sending sum:");
+        String sendingSum = reader.readLine();
+
+        Asset asset = new AssetTypeCreditAlphaNum12(assetCode, sourceKeys);
+
+        // First, the receiving account must trust the asset
+        AccountResponse receiving = server.accounts().account(destinationKeys);
+        Transaction allowAsset = new Transaction.Builder(receiving)
+                .addOperation(
+                        // The `ChangeTrust` operation creates (or alters) a trustline
+                        // The second parameter limits the amount the account can hold
+                        new ChangeTrustOperation.Builder(asset, trustLimit).build())
+                .build();
+        allowAsset.sign(destinationKeys);
+        server.submitTransaction(allowAsset);
+
+        // Second, the issuing account actually sends a payment using the asset
+        AccountResponse issuing = server.accounts().account(sourceKeys);
+        Transaction sendAsset = new Transaction.Builder(issuing)
+                .addOperation(
+                        new PaymentOperation.Builder(destinationKeys, new AssetTypeCreditAlphaNum12(assetCode, sourceKeys), sendingSum).build())
+                .build();
+        sendAsset.sign(sourceKeys);
+        server.submitTransaction(sendAsset);
+    }
+
+    private static void createAndSendCustomAsset(BufferedReader reader, Server server) throws IOException {
+        System.out.println("Enter secret seed of issuing account:");
+        KeyPair issuingKeys = KeyPair.fromSecretSeed(reader.readLine());
+        System.out.println("Enter secret seed of receiving account:");
+        KeyPair receivingKeys = KeyPair.fromSecretSeed(reader.readLine());
+        System.out.println("Enter asset name:");
+        String assetName = reader.readLine();
+        System.out.println("Enter trust limit:");
+        String trustLimit = reader.readLine();
+        System.out.println("Enter sending sum:");
+        String sendingSum = reader.readLine();
+
+        Asset asset = Asset.createNonNativeAsset(assetName, issuingKeys);
+
+        // First, the receiving account must trust the asset
+        AccountResponse receiving = server.accounts().account(receivingKeys);
+        Transaction allowAsset = new Transaction.Builder(receiving)
+                .addOperation(
+                        // The `ChangeTrust` operation creates (or alters) a trustline
+                        // The second parameter limits the amount the account can hold
+                        new ChangeTrustOperation.Builder(asset, trustLimit).build())
+                .build();
+        allowAsset.sign(receivingKeys);
+        server.submitTransaction(allowAsset);
+
+        // Second, the issuing account actually sends a payment using the asset
+        AccountResponse issuing = server.accounts().account(issuingKeys);
+        Transaction sendAsset = new Transaction.Builder(issuing)
+                .addOperation(
+                        new PaymentOperation.Builder(receivingKeys, asset, sendingSum).build())
+                .build();
+        sendAsset.sign(issuingKeys);
+        server.submitTransaction(sendAsset);
     }
 
     private static void createAccount(Server server) throws IOException {

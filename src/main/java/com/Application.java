174a9c28f1e7391999357b767e40001e13800dfa
Maxim Dribas <mvdribas@gmail.com>
2018-08-25 17:27:52 +0300

    Added: create asset; change trust; send asset/new issue.

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,7 +4,6 @@ import org.stellar.sdk.Asset;
 import org.stellar.sdk.AssetTypeNative;
 import org.stellar.sdk.ChangeTrustOperation;
 import org.stellar.sdk.KeyPair;
-import org.stellar.sdk.ManageOfferOperation;
 import org.stellar.sdk.Memo;
 import org.stellar.sdk.Network;
 import org.stellar.sdk.PaymentOperation;
@@ -17,12 +16,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Application {
-    private static Map<String, String> accountMap = new HashMap<String, String>();
     private static boolean stop = false;
 
     public static void main(String[] args) throws IOException {
@@ -41,26 +37,26 @@ public class Application {
     }
 
     private static void menu(BufferedReader reader, Server server) throws Exception {
-        System.out.println("\nClick: 1 - create an Account, 2 - send lumens, 3 - print account info, \n" +
-                "4 - print info about all accounts, 5 - create and send an asset, 0 - exit the program");
+        System.out.println("\nClick: 1 - create Account, 2 - send XLM, 3 - print account INFO, " +
+                "4 - TRUST creation, 5 - send asset, 0 - exit");
         int number = Integer.parseInt(reader.readLine());
         switch (number) {
             case 1:
                 createAccount(server);
                 break;
             case 2:
-                sendLumens(reader, server);
+                sendXLM(reader, server);
                 break;
             case 3:
-                System.out.println("Enter secret seed of source account:");
+                System.out.println("Enter secret seed of account:");
                 String secretSeed = reader.readLine();
                 printAccountInfo(secretSeed, server);
                 break;
             case 4:
-                printAllAccountsInfo(server);
+                changeTrust(reader, server);
                 break;
             case 5:
-                createAndSendCustomAsset(reader, server);
+                sendNonNativeAsset(reader, server);
                 break;
             case 0:
                 reader.close();
@@ -72,7 +68,34 @@ public class Application {
         }
     }
 
-    private static void sendLumens(BufferedReader reader, Server server) throws IOException {
+    private static void createAccount(Server server) throws IOException {
+        KeyPair keyPair = KeyPair.random();
+        String friendbotUrl = String.format("https://friendbot.stellar.org/?addr=%s", keyPair.getAccountId());
+        InputStream response = new URL(friendbotUrl).openStream();
+        String body = new Scanner(response, "UTF-8").useDelimiter("\\A").next();
+
+        System.out.print("SUCCESS account creation!");
+        printAccountInfo(new String(keyPair.getSecretSeed()), server);
+    }
+
+    private static void printAccountInfo(String secretSeed, Server server) throws IOException {
+        KeyPair keyPair = KeyPair.fromSecretSeed(secretSeed);
+        AccountResponse account = server.accounts().account(keyPair);
+
+        System.out.println("Account info: \n" + "   [account id: " + keyPair.getAccountId() +
+                "\n   secret seed: " + new String(keyPair.getSecretSeed()));
+
+        for (AccountResponse.Balance balance : account.getBalances()) {
+            System.out.println(String.format(
+                    "   [Type: %s, Code: %s, Balance: %s, Limit: %s]",
+                    balance.getAssetType(),
+                    balance.getAssetCode(),
+                    balance.getBalance(),
+                    balance.getLimit()));
+        }
+    }
+
+    private static void sendXLM(BufferedReader reader, Server server) throws IOException {
         System.out.println("Enter secret seed of source account:");
         KeyPair source = KeyPair.fromSecretSeed(reader.readLine());
         System.out.println("Enter account id of destination account:");
@@ -93,43 +116,97 @@ public class Application {
         server.submitTransaction(transaction);
     }
 
-    private static void createAndSendCustomAsset(BufferedReader reader, Server server) throws IOException {
+    private static Asset assetCreation(BufferedReader reader, Server server) throws IOException {
+        System.out.println(" * * * ASSET CREATION: * * * ");
+        System.out.println("Enter asset name:");
+        String assetName = reader.readLine();
         System.out.println("Enter secret seed of ISSUING account:");
         KeyPair issuingKeys = KeyPair.fromSecretSeed(reader.readLine());
-        AccountResponse issuingAccount = server.accounts().account(issuingKeys);
 
-        System.out.println("Enter secret seed of DISTRIBUTION account:");
-        KeyPair distributionKeys = KeyPair.fromSecretSeed(reader.readLine());
-        AccountResponse distributionAccount = server.accounts().account(distributionKeys);
+        return Asset.createNonNativeAsset(assetName, issuingKeys);
+    }
 
+    private static void changeTrust(BufferedReader reader, Server server) throws IOException {
+        System.out.println(" * * * CHANGE TRUST * * * ");
+        Asset asset = assetCreation(reader, server);
+
+        //Receiving account (prepare).
         System.out.println("Enter secret seed of RECEIVING account:");
         KeyPair receivingKeys = KeyPair.fromSecretSeed(reader.readLine());
         AccountResponse receivingAccount = server.accounts().account(receivingKeys);
 
-        System.out.println("Enter asset name:");
-        String assetName = reader.readLine();
+        System.out.println("Receiving account: ");
+        printAccountInfo(new String(receivingKeys.getSecretSeed()), server);
+
+        //Trust transaction creation.
         System.out.println("Enter trust limit:");
         String trustLimit = reader.readLine();
-        System.out.println("Enter sending sum:");
-        String sendingSum = reader.readLine();
 
-        //Asset creation.
-        Asset asset = Asset.createNonNativeAsset(assetName, issuingKeys);
-
-        //Trust creation.
-        Transaction trustAsset = new Transaction.Builder(distributionAccount)
+        Transaction trustTransaction = new Transaction.Builder(receivingAccount)
                 .addOperation(new ChangeTrustOperation.Builder(asset, trustLimit).build())
                 .build();
-        trustAsset.sign(distributionKeys);
-        server.submitTransaction(trustAsset);
+        trustTransaction.sign(receivingKeys);
+        System.out.println("Trust transaction: " + server.submitTransaction(trustTransaction).isSuccess());
 
-        //Send asset (issuing-distributor).
-        Transaction sendAsset_issuingToDistributor = new Transaction.Builder(issuingAccount)
-                .addOperation(new PaymentOperation.Builder(distributionKeys, asset, sendingSum).build())
+        //Receiving account (check changes).
+        System.out.println("Receiving account: ");
+        printAccountInfo(new String(receivingKeys.getSecretSeed()), server);
+    }
+
+    private static void sendNonNativeAsset(BufferedReader reader, Server server) throws IOException {
+        Asset asset = assetCreation(reader, server);
+
+        System.out.println(" * * * SEND NON NATIVE ASSET * * * ");
+
+        System.out.println("Enter secret seed of SOURCE account:");
+        KeyPair sourceKeys = KeyPair.fromSecretSeed(reader.readLine());
+        AccountResponse sourceAccount = server.accounts().account(sourceKeys);
+        printAccountInfo(new String(sourceKeys.getSecretSeed()), server);
+
+        System.out.println("Enter secret seed of RECEIVING account:");
+        KeyPair receivingKeys = KeyPair.fromSecretSeed(reader.readLine());
+        AccountResponse receivingAccount = server.accounts().account(receivingKeys);
+        printAccountInfo(new String(receivingKeys.getSecretSeed()), server);
+
+        //Payment transaction.
+        System.out.println("Enter sum:");
+        String sum = reader.readLine();
+
+        Transaction sendAsset = new Transaction.Builder(sourceAccount)
+                .addOperation(new PaymentOperation.Builder(receivingKeys, asset, sum).build())
                 .build();
-        sendAsset_issuingToDistributor.sign(issuingKeys);
-        server.submitTransaction(sendAsset_issuingToDistributor);
+        sendAsset.sign(sourceKeys);
+        System.out.println("Payment submit: " + server.submitTransaction(sendAsset).isSuccess());
 
+        printAccountInfo(new String(sourceKeys.getSecretSeed()), server);
+        printAccountInfo(new String(receivingKeys.getSecretSeed()), server);
+
+    }
+
+    /*
+    private static void newInflation(BufferedReader reader, Server server) throws IOException {
+        System.out.println("Enter secret seed of ISSUING account:");
+        KeyPair keyPair = KeyPair.fromSecretSeed(reader.readLine());
+        AccountResponse account = server.accounts().account(keyPair);
+
+        Transaction transInflation = new Transaction.Builder(account)
+                .addOperation(new InflationOperation())
+                .build();
+        transInflation.sign(keyPair);
+        server.submitTransaction(transInflation);
+    }
+
+    private static void setHomeDomain() {
+        Transaction setHomeDomain = new Transaction.Builder(issuingAccount)
+                .addOperation(new SetOptionsOperation.Builder()
+                        .setHomeDomain("yourdomain.com").build())
+                        .build();
+        setHomeDomain.sign(issuingKeys);
+        server.submitTransaction(setHomeDomain);
+
+    }
+
+    private static void createOffer() {
         //Offer creation.
         ManageOfferOperation firstOffer = new ManageOfferOperation.Builder(new AssetTypeNative(),
                 asset, "1", "1")
@@ -143,45 +220,7 @@ public class Application {
                 .build();
         transDistributedAsset.sign(distributionKeys);
         server.submitTransaction(transDistributedAsset);
-
-        //Payment creation (distributor-receiver)
-        Transaction sendAsset_distributorToReceiver = new Transaction.Builder(distributionAccount)
-                .addOperation(new PaymentOperation.Builder(receivingKeys, asset, "1").build())
-                .build();
-        sendAsset_distributorToReceiver.sign(distributionKeys);
-        server.submitTransaction(sendAsset_distributorToReceiver);
     }
+*/
 
-    private static void createAccount(Server server) throws IOException {
-        KeyPair keyPair = KeyPair.random();
-        String friendbotUrl = String.format("https://friendbot.stellar.org/?addr=%s", keyPair.getAccountId());
-        InputStream response = new URL(friendbotUrl).openStream();
-        String body = new Scanner(response, "UTF-8").useDelimiter("\\A").next();
-
-        System.out.print("SUCCESS account creation!");
-        printAccountInfo(new String(keyPair.getSecretSeed()), server);
-        accountMap.put(keyPair.getAccountId(), new String(keyPair.getSecretSeed()));
-    }
-
-    private static void printAccountInfo(String secretSeed, Server server) throws IOException {
-        KeyPair keyPair = KeyPair.fromSecretSeed(secretSeed);
-        AccountResponse account = server.accounts().account(keyPair);
-
-        System.out.println("\nAccount info: \n" + "   [account id: " + keyPair.getAccountId() +
-                "\n   secret seed: " + new String(keyPair.getSecretSeed()));
-
-        for (AccountResponse.Balance balance : account.getBalances()) {
-            System.out.println(String.format(
-                    "   Type: %s, Code: %s, Balance: %s]",
-                    balance.getAssetType(),
-                    balance.getAssetCode(),
-                    balance.getBalance()));
-        }
-    }
-
-    private static void printAllAccountsInfo(Server server) throws IOException {
-        for (Map.Entry<String, String> entry : accountMap.entrySet()) {
-            printAccountInfo(entry.getValue(), server);
-        }
-    }
 }
